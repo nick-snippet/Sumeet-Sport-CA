79a0664ccc856e151bbf422db19c049a25e5268f
bismilla <bismilla@upapp.co>
2025-11-22 14:54:44 +0530

    coach route

--- SS Backend/src/routes/coachesRoute.js
+++ SS Backend/src/routes/coachesRoute.js
@@ -1,10 +1,14 @@
 import express from "express";
-import upload from "../middlewares/upload.js";
-import { uploadCoach, getCoaches } from "../controller/coachescontroller.js";
+import multerUpload from "../middlewares/upload.js";
+import { uploadCoach, getCoaches } from "../controllers/coachesController.js";
+import { verifyAdmin } from "../middlewares/auth.js";
 
 const router = express.Router();
 
-router.post("/upload", upload.single("image"), uploadCoach);
-router.get("/", getCoaches);
+// upload new coach
+router.post("/upload", verifyAdmin, multerUpload.single("image"), uploadCoach);
+
+// get all coaches
+router.get("/all", getCoaches);
 
 export default router;
